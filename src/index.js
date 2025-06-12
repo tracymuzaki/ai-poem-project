050a9a2e28d6ce517ac0d2a4006e7c4595567cef
tracymuzaki <tracymuzaki12@gmail.com>
2025-06-13 01:08:33 +0300

    updated prompt

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#instructions-input");
   let apiKey = "t0ofa24ea50a4bf83aaaf293402db0a1";
-  let prompt = `You are an intelligent Assistant AI that knows many french poems for every topic in ${instructionsInput.value}. Provide the poem in the html format of "<p></p>" and conclude with a signature "<b>--SheCodes AI</b>" in bold. Make it five lines.`;
+  let prompt = `You are an intelligent Assistant AI that knows many french poems for every topic in ${instructionsInput.value}. Provide the poem in the html format of "<p></p>" and conclude with a signature "<strong>--SheCodes AI</strong>". Make it five lines.`;
   let context = "Generate one short french poem for the requested french topic";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
