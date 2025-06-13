b0ecbe7f1de1b067f4f00dac9ec11110f84b1b47
tracymuzaki <tracymuzaki12@gmail.com>
2025-06-13 20:49:54 +0300

    improved the UX

--- src/index.js
+++ src/index.js
@@ -10,8 +10,11 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#instructions-input");
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<span class="generating">Generating a French poem about ${instructionsInput.value}...</span>`;
+  poemElement.classList.remove("hidden");
   let apiKey = "t0ofa24ea50a4bf83aaaf293402db0a1";
-  let prompt = `You are an intelligent Assistant AI that knows many french poems for every topic in ${instructionsInput.value}. Provide the poem in the html format of "<p></p>" and conclude with a signature "<strong>--SheCodes AI</strong>". Make it five lines.`;
+  let prompt = `You are an intelligent Assistant AI that knows many french poems for every topic in ${instructionsInput.value}. Provide the poem in the html format of "<p></p>" with a "<br>" after each line and conclude with a signature "<strong>--SheCodes AI</strong>". Make it five lines.`;
   let context = "Generate one short french poem for the requested french topic";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
